21c77bdacc2d78aeca7030722207d3d9b40863a1
qscxzsewqazxcde@gmail.com <qscxzsewqazxcde@gmail.com>
2023-07-23 22:04:22 +0300

    Initial commit; add current website state.

---
+++ README.md
@@ -0,0 +1,27 @@
+# Aboutme
+
+Initially, this project was hosted on one of my personal laptops, and I was
+keeping it closed source to avoid giving out any possible ways of attacking it.
+Being so overwhelmed by all the fame it brought me took away all the time I was
+going to spend on setting up top-notch security (/s). Now that I moved to cloud
+providers where protection was a given, I decided to open source it, leaving
+here only the data that is also displayed on the website. Another advantage is
+that now this repository becomes the source of truth for the website, so I can
+basically:
+
+- switch computers without having to worry about syncing them too much;
+- create a basic pipeline which updates my website whenever I push;
+- get feedback more easily on my shitty HTML, CSS, Javascript trinity whilst
+  also simplifying the collaboration with other people.
+
+The source code here leaves a lot to be desired, but since this is written in
+my free time and the quest of discovering the fronted lore is not even in the
+funding phase yet, I will accept the shame. While looking at it almost brings
+a tear to my eye, I've accepted that there are more important things to focus
+on, such as my writing style, the availability and not ending up with a bigass
+bill in the mail for something that's supposed to be free (because remember,
+nothing is _really_ free). Spending time on cleaning the pillars of a building
+that's already standing is something I invested a lot on effort in learning how
+**NOT** to do.
+
+The plans for this project will be in the issues section of this repository.
---
+++ scripts/components.js
@@ -0,0 +1,81 @@
+class BusinessCard extends HTMLElement {
+	constructor() {
+		super();
+	}
+
+	connectedCallback() {
+		let path = this.getAttribute('path') ?? '';
+		this.innerHTML = `
+        <section class="profile-section">
+        <img src="${path}/assets/mothman_cut.png" alt="" class="profile-pic" />
+        <div class="profile-data">
+          <div class="name">Alex Bledea</div>
+          <div class="align-center info">
+            <div class="position">Software Engineer</div>
+            <i class="fa-solid fa-circle"></i>
+            <div class="location">Cluj-Napoca, Romania</div>
+          </div>
+          <div class="links space-around">
+            <div class="card github" onclick="openLink(\`https://github.com/Ozoniuss\`)">
+              <i class="fa-brands fa-github"></i>
+              <div class="media">GitHub</div>
+            </div>
+            <div class="card linkedin" onclick="openLink(\`https://www.linkedin.com/in/alex-bledea-a8865b1a0/\`)">
+              <i class="fa-brands fa-linkedin"></i>
+              <div class="media">LinkedIn</div>
+            </div>
+            <div class="card medium" onclick="openLink(\`https://medium.com/@ozoniuss\`)">
+              <i class="fa-brands fa-medium"></i>
+              <div class="media">Medium</div>
+            </div>
+          </div>
+        </div>
+      </section>
+`;
+	}
+}
+
+class NavMenu extends HTMLElement {
+	constructor() {
+		super();
+	}
+
+	connectedCallback() {
+		let current = this.getAttribute('current');
+		let path = this.getAttribute('path') ?? '';
+		this.innerHTML = `
+<div class="header space-around">
+    <a href="${path}/index.html" class="tab ${
+			current === 'home' ? 'is-active' : ''
+		}">Home</a>
+    <a href="${path}/about/index.html" class="tab ${
+			current === 'about' ? 'is-active' : ''
+		}">About me</a>
+    <a href=${path}/guitar/index.html class="tab ${
+			current === 'guitar' ? 'is-active' : ''
+		}">Guitar</a>
+    <a href=${path}/blog/index.html class="tab ${
+			current === 'blog' ? 'is-active' : ''
+		}">Blog</a>
+    <a href="${path}/resume-downtime/index.html" target="_blank" class="tab">Resume</a>
+</div>
+`;
+	}
+}
+
+class ModifiedAtItem extends HTMLElement {
+	constructor() {
+		super();
+	}
+
+	connectedCallback() {
+		let currentDate = document.lastModified;
+		this.innerHTML = `
+    <p>Modified at ${currentDate}.</p>
+    `;
+	}
+}
+
+customElements.define('business-card', BusinessCard);
+customElements.define('nav-menu', NavMenu);
+customElements.define('modified-at-item', ModifiedAtItem);
