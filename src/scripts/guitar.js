f0928230df24f02981585d53d74570fba8381ee0
Ozoniuss <qscxzsewqazxcde@gmail.com>
2026-01-10 17:11:58 +0200

    Add music

---
+++ src/scripts/guitar.js
@@ -0,0 +1,10 @@
+document.addEventListener("play", function (e) {
+  const audios = document.querySelectorAll("audio");
+
+  audios.forEach(audio => {
+    if (audio !== e.target) {
+      audio.pause();
+      audio.currentTime = 0;
+    }
+  });
+}, true);
